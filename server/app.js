b86fd1a4f57ec5cba781f9a70209b6210751bee0
Abubakar Shaikh <abubakarshaikhask@gmail.com>
2023-10-10 22:18:06 +0530

    Completed SocketIO, game logic

--- server/app.js
+++ server/app.js
@@ -1,11 +1,16 @@
 const path = require("path");
 const express = require("express");
 const cors = require("cors");
-require("./database/mongoose");
+const { connectToDB } = require("./database/db");
 
 const publicDirectoryPath = path.join(__dirname, "../public");
 const app = express();
 
+(async () => {
+  await connectToDB();
+})();
+
+// middlewares
 app.use(express.json());
 app.use(cors());
 app.use(express.static(publicDirectoryPath));
